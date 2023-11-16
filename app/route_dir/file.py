5ebbc4505f57cab902fac41dbe881bd908c3284a
Yannick Simon <yannick.simon@gmail.com>
2023-11-16 22:22:29 +0100

    File

--- app/route_dir/file.py
+++ app/route_dir/file.py
@@ -24,95 +24,13 @@ import piexif
 from fractions import Fraction
 
 
-ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
+ALLOWED_EXTENSIONS = {'pdf'}
 
 # Setup upload folder
 UPLOAD_FOLDER = config["upload_dir"]
 
 # cf fileupload : https://flask.palletsprojects.com/en/2.2.x/patterns/fileuploads/
 
-
-def deg_to_dms(decimal_coordinate, cardinal_directions):
-    """
-    This function converts decimal coordinates into the DMS (degrees, minutes and seconds) format.
-    It also determines the cardinal direction of the coordinates.
-
-    :param decimal_coordinate: the decimal coordinates, such as 34.0522
-    :param cardinal_directions: the locations of the decimal coordinate, such as ["S", "N"] or ["W", "E"]
-    :return: degrees, minutes, seconds and compass_direction
-    :rtype: int, int, float, string
-    """
-    if decimal_coordinate < 0:
-        compass_direction = cardinal_directions[0]
-    elif decimal_coordinate > 0:
-        compass_direction = cardinal_directions[1]
-    else:
-        compass_direction = ""
-    degrees = int(abs(decimal_coordinate))
-    decimal_minutes = (abs(decimal_coordinate) - degrees) * 60
-    minutes = int(decimal_minutes)
-    seconds = Fraction((decimal_minutes - minutes) * 60).limit_denominator(100)
-    return degrees, minutes, seconds, compass_direction
-
-def dms_to_exif_format(dms_degrees, dms_minutes, dms_seconds):
-    """
-    This function converts DMS (degrees, minutes and seconds) to values that can
-    be used with the EXIF (Exchangeable Image File Format).
-
-    :param dms_degrees: int value for degrees
-    :param dms_minutes: int value for minutes
-    :param dms_seconds: fractions.Fraction value for seconds
-    :return: EXIF values for the provided DMS values
-    :rtype: nested tuple
-    """
-    exif_format = (
-        (dms_degrees, 1),
-        (dms_minutes, 1),
-        (int(dms_seconds.limit_denominator(100).numerator), int(dms_seconds.limit_denominator(100).denominator))
-    )
-    return exif_format
-
-
-def add_geolocation(image_path, latitude, longitude):
-    """
-    This function adds GPS values to an image using the EXIF format.
-    This fumction calls the functions deg_to_dms and dms_to_exif_format.
-
-    :param image_path: image to add the GPS data to
-    :param latitude: the north–south position coordinate
-    :param longitude: the east–west position coordinate
-    """
-    # converts the latitude and longitude coordinates to DMS
-    latitude_dms = deg_to_dms(latitude, ["S", "N"])
-    longitude_dms = deg_to_dms(longitude, ["W", "E"])
-
-    # convert the DMS values to EXIF values
-    exif_latitude = dms_to_exif_format(latitude_dms[0], latitude_dms[1], latitude_dms[2])
-    exif_longitude = dms_to_exif_format(longitude_dms[0], longitude_dms[1], longitude_dms[2])
-
-    try:
-        # Load existing EXIF data
-        exif_data = piexif.load(image_path)
-
-        # https://exiftool.org/TagNames/GPS.html
-        # Create the GPS EXIF data
-        coordinates = {
-            piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),
-            piexif.GPSIFD.GPSLatitude: exif_latitude,
-            piexif.GPSIFD.GPSLatitudeRef: latitude_dms[3],
-            piexif.GPSIFD.GPSLongitude: exif_longitude,
-            piexif.GPSIFD.GPSLongitudeRef: longitude_dms[3]
-        }
-
-        # Update the EXIF data with the GPS information
-        exif_data['GPS'] = coordinates
-
-        # Dump the updated EXIF data and insert it into the image
-        exif_bytes = piexif.dump(exif_data)
-        piexif.insert(exif_bytes, image_path)
-        print(f"EXIF data updated successfully for the image {image_path}.")
-    except Exception as e:
-        print(f"Error: {str(e)}")
 
 
 
@@ -133,12 +51,6 @@ def create_file():
         
     if not 'file_on_site_uuid' in request.form:
         abort(make_response(jsonify(error="missing file_on_site_uuid parameter"), 400))
-
-    if not 'latitude' in request.form:
-        abort(make_response(jsonify(error="missing latitude parameter"), 400))
-        
-    if not 'longitude' in request.form:
-        abort(make_response(jsonify(error="missing longitude parameter"), 400))
 
     if not 'intervention_on_site_uuid' in request.form:
         abort(make_response(jsonify(error="missing intervention_on_site_uuid parameter"), 400))
@@ -168,7 +80,6 @@ def create_file():
     newfilename                 = file_on_site_uuid+get_extension(filename)
     
     file.save(os.path.join(UPLOAD_FOLDER, newfilename))
-    add_geolocation(os.path.join(UPLOAD_FOLDER, newfilename), float(latitude), float(longitude))
     
     file = File(  file_on_site_uuid=file_on_site_uuid,
                     filename= newfilename, 
