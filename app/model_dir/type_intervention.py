24657359994123042c8bec99a7e94f4513e620a9
Yannick Simon <yannick.simon@gmail.com>
2023-12-29 08:44:36 +0100

    add types_interventions_organizations

--- app/model_dir/type_intervention.py
+++ app/model_dir/type_intervention.py
@@ -4,7 +4,14 @@ from .mymixin import MyMixin
 from sqlalchemy.orm import declarative_base, relationship, backref
 import uuid
 
-
+class TypeInterventionOrganization(db.Model):
+    __tablename__ = 'types_interventions_organizations'
+    
+    type_intervention_id= db.Column(db.String(36), db.ForeignKey('types_interventions.id'), primary_key=True)
+    organization_id=db.Column(db.String(36), db.ForeignKey('organizations.id'), primary_key=True)
+    config_text=db.Column(db.Text)
+    
+    
 class TypeIntervention(db.Model, MyMixin):
     
     __tablename__ = 'types_interventions'
